6cc54a1c25cd6d4883df5aafc605f0520269ac6d
Vikash <vikashdas770@gmail.com>
2024-05-16 14:20:51 +0530

    initial project structure ready

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.34.37","langchain_community","chromadb","awscli",
+"streamlit",
+"pypdf",
+"faiss-cpu"]
+)
